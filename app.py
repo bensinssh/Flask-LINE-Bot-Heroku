51500f504989ebf7ad8eb99abd5972798aa3ea43
menuflex <79350810+bensinssh@users.noreply.github.com>
2021-11-04 01:56:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,6 @@ def reply(intent,text,reply_token,id,disname):
             message
         )
 
-''​'
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
